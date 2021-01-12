ade2bbeb0e6534e21e652d999df3aaa9436c50b6
dev <dev@ya.ru>
2021-01-13 00:52:52 +0300

    add new programs

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,40 @@
+public class Calculator{
+	public static void main(String[] args){
+		int firstNum = 5;
+		int secondNum = 3;
+		char plus = '+';
+		char minus = '-';
+		char proizved = '*';
+		char del = '/';
+		char step = '^';
+		char ost = '%';		
+		if(plus == '+'){
+			int sum = firstNum + secondNum;
+			System.out.println("Сумма чисел = " + sum);
+		}
+		if(minus == '-'){
+			int vich = firstNum - secondNum;
+			System.out.println("Разность чисел = " + vich);
+		}
+		if(proizved == '*'){
+			int pro = firstNum * secondNum;
+			System.out.println("Произведение чисел = " + pro);
+		}
+		if(del == '/'){
+			int dele = firstNum / secondNum;
+			System.out.println("Частное чисел = " + dele);
+		}
+		if(step == '^'){
+			int stepe = firstNum;
+			for(int i = 1; i < secondNum; i++){
+				stepe = stepe * firstNum;
+			}
+			System.out.println("Возведение в степень одного числа на другое = " + stepe);
+		}
+		if(ost == '%'){
+			int osta = firstNum % secondNum;
+			System.out.println("Остаток от деления чисел = " + osta);
+		}
+		
+	}
+}
---
+++ Lesson_1/Ugad.java
@@ -0,0 +1,16 @@
+public class Ugad{
+	public static void main(String[] args){
+		int zagad = 14;
+		int i = 1;
+		while(true){
+			if(i == zagad){
+				System.out.println("Вы угадали число!");
+				break;
+			}
+			else{
+				System.out.println("Вы не угадали число!");
+			}
+			i++;
+		}
+	}
+}
